e13b376fe8624475b180a4f1be200b2b3e98e455
Rahul Sriram Esakkimuthu <81294511+RaHuL-SrIrAM-E@users.noreply.github.com>
2025-04-15 14:31:50 +0400

    initial commit

---
+++ appts-by-vas/src/main/java/com/wizard/appts_by_vas/ApptsByVasApplication.java
@@ -0,0 +1,13 @@
+package com.wizard.appts_by_vas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApptsByVasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApptsByVasApplication.class, args);
+	}
+
+}
---
+++ appts-by-vas/src/test/java/com/wizard/appts_by_vas/ApptsByVasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wizard.appts_by_vas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApptsByVasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
